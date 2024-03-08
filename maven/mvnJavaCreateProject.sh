b3dc07778482baca7063324969d7e6af07506e3e
tarkancode <tarkancode@gmail.com>
2024-03-08 21:07:02 +0100

    Update script

--- maven/mvnJavaCreateProject.sh
+++ maven/mvnJavaCreateProject.sh
@@ -1,1 +1,7 @@
-mvn archetype:generate -DgroupId=tarkan.bitsandpieces -DartifactId=script -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
+#!/bin/bash
+if [ "$#" -ne 2 ]; then
+    echo "Missing args!"
+    echo "Usage: $0 <groupId> <artifactId>"
+    exit 1
+fi
+mvn archetype:generate -DgroupId=$1 -DartifactId=$2 -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
