0b3ceca0d473daa0735b0171c38fceb25657da32
tarkancode <tarkancode@gmail.com>
2024-03-08 21:08:30 +0100

    Remove obselete file

--- maven/mvnJavaCreateProject.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ "$#" -ne 2 ]; then
-    echo "Missing args!"
-    echo "Usage: $0 <groupId> <artifactId>"
-    exit 1
-fi
-mvn archetype:generate -DgroupId=$1 -DartifactId=$2 -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
